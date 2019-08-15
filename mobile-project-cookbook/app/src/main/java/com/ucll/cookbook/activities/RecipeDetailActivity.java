7e1854cbe78e54d6760cf5095c792fd638bf43a0
Sam Janssens <sam.janssens@student.ucll.be>
2019-08-15 17:22:05 +0200

    recipedetaillist

---
+++ mobile-project-cookbook/app/src/main/java/com/ucll/cookbook/activities/RecipeDetailActivity.java
@@ -0,0 +1,119 @@
+package com.ucll.cookbook.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ucll.cookbook.R;
+import com.ucll.cookbook.domain.model.Recipe;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+    private ImageView ivBookCover;
+    private TextView tvTitle;
+    private TextView tvAuthor;
+    private TextView tvPublisher;
+    private TextView tvPageCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+        // Fetch views
+        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+        tvPublisher = (TextView) findViewById(R.id.tvPublisher);
+        tvPageCount = (TextView) findViewById(R.id.tvPageCount);
+        // Use the book to populate the data into our views
+        Recipe recipe = (Recipe) getIntent().getSerializableExtra(RecipeListActivity.Recipe_DETAIL_KEY);
+        loadRecipe(recipe);
+    }
+
+    private void loadRecipe(Recipe recipe) {
+        //change activity title
+        this.setTitle(recipe.getName());
+        // Populate data
+        tvTitle.setText(recipe.getName());
+        tvAuthor.setText(recipe.getDescription());
+        // fetch extra book data from books API
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recipe_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            setShareIntent();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setShareIntent() {
+        ImageView ivImage = (ImageView) findViewById(R.id.ivBookCover);
+        final TextView tvTitle = (TextView)findViewById(R.id.tvTitle);
+        // Get access to the URI for the bitmap
+        Uri bmpUri = getLocalBitmapUri(ivImage);
+        // Construct a ShareIntent with link to image
+        Intent shareIntent = new Intent();
+        // Construct a ShareIntent with link to image
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("*/*");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, (String)tvTitle.getText());
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        // Launch share menu
+        startActivity(Intent.createChooser(shareIntent, "Share Image"));
+
+    }
+
+    // Returns the URI path to the Bitmap displayed in cover imageview
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+}
+
